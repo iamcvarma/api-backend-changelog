c166322a3a8737efccc8c6beb8d19396bb132f5d
Chaitanya <iamcvers@gmail.com>
2022-11-18 01:34:03 +0530

    added migration and router

---
+++ src/router.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+
+const router = Router();
+
+// Product
+
+router.get("/product", (req, res) => {
+  res.send("hellos");
+});
+router.get("/product/:id", () => {});
+router.post("/product", () => {});
+router.put("/product/:id", () => {});
+router.delete("/product/:id", () => {});
+
+// Updates
+
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.post("/update", () => {});
+router.put("/update/:id", () => {});
+router.delete("/update/:id", () => {});
+
+// update updatepoint
+
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.post("/updatepoint", () => {});
+router.put("/updatepoint/:id", () => {});
+router.delete("/updatepoint/:id", () => {});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import express from "express";
+import router from "./router";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("ehllosdf");
-});
+app.use("/api", router);
 
 export default app;
